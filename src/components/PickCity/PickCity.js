e2c5a117b45f578a39fcaf9ecd54b4fddc980576
Karina Penkala <marsz@poczta.onet.pl>
2023-05-01 23:16:06 +0200

    handleSubmit PickCity

--- src/components/PickCity/PickCity.js
+++ src/components/PickCity/PickCity.js
@@ -7,12 +7,18 @@ import { useState } from 'react';
 const PickCity = () => {
   const [city, setCity] = useState('');
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    action({city});
+    setCity('');
+  };
+
   return (
-    <form className={styles.pickCityForm}> 
+    <form className={styles.pickCityForm} onSubmit={handleSubmit}> 
       <label>
         <TextInput placeholder="Enter city name...." value={city} onChange={e => setCity(e.target.value)} />
       </label>
-      <Button>Search</Button>
+      <Button onSubmit={handleSubmit}>Search</Button>
     </form>
   );
 };
