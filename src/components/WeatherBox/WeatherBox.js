2b728f8418cefbeb32f22bd4326d25be1d31855d
Karina Penkala <marsz@poczta.onet.pl>
2023-05-02 13:14:24 +0200

    add fetch

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,11 +1,22 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import { useCallback } from 'react';
 
 const WeatherBox = props => {
+  const handleCityChange = useCallback(city => {
+    console.log(city);
+  });
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${handleCityChange}&appid=7a9cee65673e9f0fb7b6ebccf43d7980
+&units=metric`)
+   .then(res => res.json())
+   .then(data => {
+     console.log(data);
+   });
+
   return (
     <section>
-      <PickCity />
+      <PickCity action={handleCityChange} />
       <WeatherSummary />
       <Loader />
     </section>
