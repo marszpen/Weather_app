e6fd3d882d83e330df3377e50e75ef1a532e79e4
Karina Penkala <marsz@poczta.onet.pl>
2023-05-03 08:11:51 +0200

    fix fetch

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -7,7 +7,7 @@ const WeatherBox = props => {
   const handleCityChange = useCallback(city => {
     console.log(city);
   });
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${handleCityChange}&appid=7a9cee65673e9f0fb7b6ebccf43d7980
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7a9cee65673e9f0fb7b6ebccf43d7980
 &units=metric`)
    .then(res => res.json())
    .then(data => {
