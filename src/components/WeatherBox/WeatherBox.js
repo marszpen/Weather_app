55525c608fe20a8f0739b0e68ea976a63e104801
Karina Penkala <marsz@poczta.onet.pl>
2023-05-03 16:35:36 +0200

    add ErrorBox

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -2,38 +2,48 @@ import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
 import { useCallback, useState } from 'react';
+import ErrorBox from '../ErrorBox/ErrorBox'
 
 const WeatherBox = props => {
   const [weather, setWeather] = useState('');
   const [load, setLoading] = useState(false);
+  const [showError, setShowError] = useState(false);
 
   const handleCityChange = useCallback(city => {
     setLoading(true);
+    setShowError(false)
     console.log(city);
-  
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7a9cee65673e9f0fb7b6ebccf43d7980&units=metric`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
 
-      const weatherData = {
-        city: data.name,
-        temp: data.main.temp,
-        icon: data.weather[0].icon,
-        description: data.weather[0].main
-      };
-      setLoading(false)
-      setWeather(weatherData)
-      console.log(weatherData);
-    });
-});
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7a9cee65673e9f0fb7b6ebccf43d7980&units=metric`)
+      .then(res => {
+        if (res.status === 200) {
+          return res.json()
+            .then(data => {
+              console.log(data);
+
+              const weatherData = {
+                city: data.name,
+                temp: data.main.temp,
+                icon: data.weather[0].icon,
+                description: data.weather[0].main
+              };
+              setLoading(false)
+              setWeather(weatherData)
+              console.log(weatherData);
+            })
+        } else {
+          setShowError(true)
+        };
+      })
+  });
 
   return (
     <section>
       <PickCity action={handleCityChange} />
       {weather && !load && <WeatherSummary {...weather} />}
-			{load && <Loader />}
+      {load && <Loader />}
       <Loader />
+      {showError && <ErrorBox />}
     </section>
   )
 };
